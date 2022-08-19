c6f9b4e6a838f83bb9f6e1274e8a25b3b848610a
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-19 18:45:58 -0500

    Start of conversion for Weapon.cs

---
+++ scripts/Weapon.cs
@@ -0,0 +1,97 @@
+using Godot;
+using System;
+
+namespace TopDownGame;
+
+public class Weapon : Node
+{
+    private string _jsonStr = "";
+    private File _jsonFile = new File();
+    private JSONParseResult _data;
+
+    //var unlocked = []
+    private WeaponType _cur = WeaponType.Fists;
+
+    private string _curName = "";
+    private int _curDamage;
+    private int _curFirerate;
+    private int _curProjectileSpeed;
+    private string _curType = "";
+    private int _curRange;
+    private int _curReload;
+
+    /*
+    #stores all weapons max clipzises and ammunition 
+    var max_ammo = []
+    var max_clipsize = []
+    #stores current ammo and clipsize usage
+    var current_ammo = []
+    var current_clipsize = []
+    */
+
+    public override void _Ready()
+    {
+        _jsonFile.Open("res://Json/weapon.json", File.ModeFlags.ReadWrite);
+        _jsonStr = _jsonFile.GetAsText();
+        _data = JSON.Parse(_jsonStr);
+
+        //for (int i = 0; i < _data.????)
+        /*if i["type"] == "melee":
+			continue
+		max_ammo.append(i["ammo"])
+		max_clipsize.append(i["clipsize"])*/
+
+        // current_ammo = max_ammo
+	    // current_clipsize = max_clipsize
+    }
+
+    public override void _Process(float delta)
+    {
+        /*switch (_cur) 
+        {
+            case WeaponType.Glock:
+				var dict = data.result[0];
+				_curDamage   = dict["dmg"];
+				_curFirerate = dict["firerate"];
+				_curType     = dict["type"];
+				_curRange    = dict["range"];
+				_curName     = dict["name"];
+				_curReload   = dict["reload_time"];
+                break;
+			case WeaponType.Bow:
+				var dict = data.result[1];
+				_curDamage = dict["dmg"];
+				_curFirerate = dict["firerate"];
+				_curProjectileSpeed = dict["bolt_speed"];
+				_curType = dict["type"];
+				_curRange = dict["range"];
+				_curName = dict["name"];
+				_curReload = dict["reload_time"];
+                break;
+			case WeaponType.Fists:
+				var dict = data.result[2];
+				_curDamage = dict["dmg"];
+				_curFirerate = dict["firerate"];
+				_curType = dict["type"];
+				_curRange = dict["range"];
+				_curName = dict["name"];
+                break;
+			case WeaponType.Bat:
+				var dict = data.result[3];
+				_curDamage = dict["dmg"];
+				_curFirerate = dict["firerate"];
+				_curType = dict["type"];
+				_curRange = dict["range"];
+				_curName = dict["name"];
+                break;
+        }*/
+    }
+}
+
+public enum WeaponType 
+{
+    Glock,
+    Bow,
+    Fists,
+    Bat
+}
