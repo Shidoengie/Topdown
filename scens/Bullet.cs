6a2253e0d0b733a59e706747b39812ed6b612c30
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-19 18:56:39 -0500

    Start of conversion for Bullet.cs

---
+++ scens/Bullet.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+namespace TopDownGame;
+
+public class Bullet : Area2D
+{
+    [Export] public int Speed = 750;
+    [Export] public int Damage = 50;
+
+    private Vector2 _velocity;
+    private Vector2 _dir;
+
+    public override void _Ready()
+    {
+        _dir = Transform.x;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Position += _dir * Speed * delta;
+    }
+
+    /*
+    var i =false 
+    func _on_Bullet_body_entered(body):
+        if not body is TileMap:
+            body.health -= damage
+            queue_free()
+            body.current_state = 2
+            return
+        queue_free()
+        */
+}
