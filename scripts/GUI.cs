fc760f963fbed921b81ecbe56423271fdca24d18
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-19 18:48:10 -0500

    Start of conversion for GUI.cs

---
+++ scripts/GUI.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+namespace TopDownGame;
+
+public class GUI : CanvasLayer
+{
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Process(float delta)
+    {
+        //$HP/Label.text = str(Stats.player_health)
+	
+        //$Weapon/Label3.text = Weapon.current_name
+        /*if Weapon.current_type != "melee":
+            $Ammo/Label3.text = str(Weapon.current_ammo[Weapon.current])
+            $Ammo/Label4.text = str(Weapon.current_clipsize[Weapon.current])
+        else:
+            $Ammo.hide()
+        if Input.is_action_just_pressed("console"):
+            $Console.show()
+        match $Console/TextEdit.text:
+            "weapons":
+                Weapon.unlocked = [Weapon.BOW,Weapon.GLOCK,Weapon.FISTS]*/
+    }
+}
