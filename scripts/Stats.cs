45017534f3c1112a922497e83b777f4abe67b7c6
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-08-19 19:49:17 -0500

    add more global usings

--- scripts/Stats.cs
+++ scripts/Stats.cs
@@ -1,5 +1,13 @@
-global using System;
 global using Godot;
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.Concurrent;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Threading;
+global using System.Text.RegularExpressions;
+global using System.Threading.Tasks;
+global using System.Linq;
 
 namespace TopDownGame;
 
