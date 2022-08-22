78a72a6f1e38a82d8053c24dfe53845541ac08c1
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-08-22 12:57:18 +0100

    Added basic enemy to player pathfinding

--- scens/enemie/Enemy.gd
+++ scens/enemie/Enemy.gd
@@ -6,44 +6,76 @@ onready var ray3 = get_node("RayCast2D3")
 onready var timer = get_node("Timer")
 
 onready var player = get_parent().get_parent().find_node("Player") as KinematicBody2D
+onready var nav = get_parent().get_parent().find_node("Navigation2D") as Navigation2D
+
 var velocity = Vector2.ZERO
+var path = []
+
+export var walkspeed = 10
 export var health = 200
 
 enum {SEARCH = 0,HUNT =1,HIT = 2}
 var current_state = SEARCH
 var seeing_player = false
 var been_hit = false
+var path_update = true
 
 func _process(delta):
+	
 	if health < 1:
 		queue_free()
 	
 	# State Machine
 	match current_state:
-		
 		# Search
 		0:
 			pass
 		# Hunt
-		# NOTE: replace position setting with 
+		# NOTE: replace teleport with actual walking
 		1:
-			var ray_arr = [ray1.get_collider(),ray2.get_collider(),ray3.get_collider()]
-			if not (player in ray_arr):
-				seeing_player = false
-			else:
-				seeing_player = true
-			if seeing_player or been_hit: 
-				look_at(player.global_position)
-				been_hit = false
-				var player_distance = global_position.distance_to(player.position)
-				if player_distance > 200:
-					position -= -player.global_position.normalized()*100 * delta
-			elif not seeing_player:
-				position = player.global_position
+			hunt_func(delta)
 		# hit
 		2:
 			been_hit = true
 			current_state = HUNT
+
+
+func hunt_func(delta):
+	var ray_arr = [ray1.get_collider(),ray2.get_collider(),ray3.get_collider()]
+	
+	if not (player in ray_arr):
+		seeing_player = false
+	else:
+		seeing_player = true
+		
+	if seeing_player or been_hit:
+		look_at(player.global_position)
+		been_hit = false
+		path_update = true
+		var player_distance = global_position.distance_to(player.position)
+		
+		if player_distance > 200:
+			path = nav.get_simple_path(global_position,player.global_position)
+			move_along_path(walkspeed*delta)
+	
+	elif not seeing_player:
+		if path_update:
+			path = nav.get_simple_path(global_position,player.global_position)
+			path_update = false
+		move_along_path(walkspeed*delta)
+
+func move_along_path(distance):
+	var last_point = position
+	while path.size() > 1:
+		var distance_between_points = last_point.distance_to(path[1])
+		if distance <= distance_between_points:
+			position = last_point.linear_interpolate(path[1], distance / distance_between_points)
+			return
+		distance -= distance_between_points
+		last_point = path[1]
+		path.remove(1)
+	position = last_point
+
 
 func _on_Area2D_body_entered(body):
 	if body == player:
@@ -55,7 +87,6 @@ func _on_Area2D_body_exited(body):
 	if body == player:
 		timer.start()
 		seeing_player = false
-
 
 func _on_Timer_timeout():
 	current_state = SEARCH
