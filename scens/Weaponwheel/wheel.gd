f0d235e6c908458ed974177af4ef6aecc99c14dc
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-09-13 15:32:51 +0100

    wp wheel still not workin

--- scens/Weaponwheel/wheel.gd
+++ scens/Weaponwheel/wheel.gd
@@ -2,44 +2,51 @@ extends Control
 
 var wheel_index = 0
 var wps_on_wheel = 1
+var total_weapons = 10
+var page_total 
 var pagecount = 0
-var page
+var cur_page = 1
 var selected
 onready var wheel_cells =  [$wheelcel0,$wheelcel1,$wheelcel2,$wheelcel3,$wheelcel4,$wheelcel5,$wheelcel6,$wheelcel7]
 #read player weapons
 #determine pagecount
 #wheel input
 func _ready():
-	update_wheel(7)
+	update_wheel(7,100)
+	page_total = total_weapons
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.is_pressed():
-			
-			if event.button_index == BUTTON_WHEEL_UP:
-				wheel_index += 1
-				if wheel_index > wps_on_wheel:
-					wheel_index = 0
-					if pagecount > 0:
-						page_up()
-				selected = wheel_cells[wheel_index]
-				selected.pressed = true
-				check_pressed()
-			if event.button_index == BUTTON_WHEEL_DOWN:
-				wheel_index -= 1
-				if wheel_index < 0:
-					wheel_index = wps_on_wheel
-					if pagecount > 0:
-						page_down()
-				selected = wheel_cells[wheel_index]
-				selected.pressed = true
-				check_pressed()
-func update_wheel(value):
+		if !event.is_pressed():
+			return
+		if event.button_index == BUTTON_WHEEL_UP:
+			wheel_index += 1
+			if wheel_index > wps_on_wheel:
+				wheel_index = 0
+				if pagecount > 0:
+					page_up()
+			selected = wheel_cells[wheel_index]
+			selected.pressed = true
+			check_pressed()
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			wheel_index -= 1
+			if wheel_index < 0:
+				wheel_index = wps_on_wheel
+				if pagecount > 0:
+					page_down()
+			selected = wheel_cells[wheel_index]
+			selected.pressed = true
+			check_pressed()
+func update_wheel(value,total:float):
+	total_weapons = total
+	pagecount = ceil(total / wheel_cells.size())
 	wps_on_wheel = value
 	for i in wheel_cells:
 		i.disabled = false
+	update_page_label()
 	if wps_on_wheel >= wheel_cells.size()-1: return
 	for i in wheel_cells.slice(wps_on_wheel+1,wheel_cells.size()-1):
 		i.disabled = true
+	
 func check_pressed():
 	var smth = wheel_cells.duplicate()
 	smth.remove(wheel_index)
@@ -47,9 +54,34 @@ func check_pressed():
 		i.pressed = false
 #placeholders
 func page_up():
-	pass
+	if page_total - 7 > 0:
+		print("a")
+		page_total -= 7
+		update_wheel(7,total_weapons)
+	else:
+		update_wheel(page_total,total_weapons)
+	cur_page += 1
+	if cur_page > pagecount:
+		cur_page = 1
+		page_total = total_weapons
+	print(page_total)
+	print(total_weapons)
+	update_page_label()
 func page_down():
-	pass
+	if page_total < 7:
+		page_total += page_total
+		update_wheel(page_total,total_weapons)
+	else:
+		update_wheel(page_total,total_weapons)
+	cur_page -= 1
+	if cur_page <= 0:
+		cur_page = pagecount
+	
+	page_total = total_weapons - wheel_cells.size()*cur_page
+	print(page_total)
+	update_page_label()
+func update_page_label()-> void:
+	$Page_label.text = str(cur_page)+"/"+str(pagecount)
 func _on_wheelcel0_pressed():
 	wheel_index = 0
 	check_pressed()
