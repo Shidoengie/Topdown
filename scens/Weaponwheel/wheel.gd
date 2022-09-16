eda9a783c8e6808118b961d849d8b670e5ecf312
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-09-16 19:02:52 +0100

    weeel

--- scens/Weaponwheel/wheel.gd
+++ scens/Weaponwheel/wheel.gd
@@ -3,7 +3,7 @@ extends Control
 var wheel_index = 0
 var wps_on_wheel = 1
 var total_weapons = 10
-var page_total 
+var page_total = 0
 var pagecount = 0
 var cur_page = 1
 var selected
@@ -13,17 +13,12 @@ onready var wheel_cells =  [$wheelcel0,$wheelcel1,$wheelcel2,$wheelcel3,$wheelce
 #wheel input
 func _ready():
 	update_wheel(7,100)
-	page_total = total_weapons
 func _input(event):
-	if event is InputEventMouseButton:
-		if !event.is_pressed():
-			return
+	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == BUTTON_WHEEL_UP:
 			wheel_index += 1
 			if wheel_index > wps_on_wheel:
 				wheel_index = 0
-				if pagecount > 0:
-					page_up()
 			selected = wheel_cells[wheel_index]
 			selected.pressed = true
 			check_pressed()
@@ -31,22 +26,28 @@ func _input(event):
 			wheel_index -= 1
 			if wheel_index < 0:
 				wheel_index = wps_on_wheel
-				if pagecount > 0:
-					page_down()
 			selected = wheel_cells[wheel_index]
 			selected.pressed = true
 			check_pressed()
+	if event is InputEventKey:
+		if Input.is_action_just_pressed("wheel_up"):
+			page_up()
+		elif Input.is_action_just_pressed("wheel_down"):
+			page_down()
 func update_wheel(value,total:float):
 	total_weapons = total
 	pagecount = ceil(total / wheel_cells.size())
 	wps_on_wheel = value
+	update_page_label()
+	update_slots()
+
+func update_slots():
 	for i in wheel_cells:
 		i.disabled = false
-	update_page_label()
 	if wps_on_wheel >= wheel_cells.size()-1: return
 	for i in wheel_cells.slice(wps_on_wheel+1,wheel_cells.size()-1):
 		i.disabled = true
-	
+
 func check_pressed():
 	var smth = wheel_cells.duplicate()
 	smth.remove(wheel_index)
@@ -54,32 +55,32 @@ func check_pressed():
 		i.pressed = false
 #placeholders
 func page_up():
-	if page_total - 7 > 0:
-		print("a")
-		page_total -= 7
-		update_wheel(7,total_weapons)
+	print(page_total)
+	page_total += wheel_cells.size()
+	if page_total > total_weapons:
+		wps_on_wheel = page_total-total_weapons
 	else:
-		update_wheel(page_total,total_weapons)
+		wps_on_wheel = wheel_cells.size()-1
+	update_slots()
 	cur_page += 1
 	if cur_page > pagecount:
 		cur_page = 1
-		page_total = total_weapons
-	print(page_total)
-	print(total_weapons)
 	update_page_label()
-func page_down():
-	if page_total < 7:
-		page_total += page_total
-		update_wheel(page_total,total_weapons)
-	else:
-		update_wheel(page_total,total_weapons)
-	cur_page -= 1
-	if cur_page <= 0:
-		cur_page = pagecount
 	
-	page_total = total_weapons - wheel_cells.size()*cur_page
-	print(page_total)
+func page_down():
+	page_total = abs(page_total)
+	if page_total > total_weapons:
+		wps_on_wheel = page_total-total_weapons
+		page_total -= (page_total-total_weapons)
+	else:
+		page_total -= wheel_cells.size()
+		wps_on_wheel = wheel_cells.size()-1
+	update_slots()
+	cur_page -= 1
+	if cur_page < 1:
+		cur_page = pagecount
 	update_page_label()
+	print(page_total)
 func update_page_label()-> void:
 	$Page_label.text = str(cur_page)+"/"+str(pagecount)
 func _on_wheelcel0_pressed():
